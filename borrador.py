e2922ef62663ab0bc7855d1132f140e18b74e0fa
JanNavart22 <u20202191756@usco.edu.co>
2023-08-08 09:31:07 -0500

    Borrador_python

---
+++ borrador.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls")
+
+# BORRADOR
+
+print()
+print("Programa de conteo")
+print()
+
+x = int((input("Ingrese el numero desde el cual inicia el conteo :")))
+y = int((input("ingrese hasta que numero desea que llege el conteo: ")))
+
+print(x)
+
+while x<y:
+    x = x+1
+    print(x)
